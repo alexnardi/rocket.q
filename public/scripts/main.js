f8d92f32ad5ade30a2f05b1270fc1586b9d6094d
alexnardi <74261712+alexnardi@users.noreply.github.com>
2021-12-07 23:39:41 -0300

    Routes, controllers and configured, ready for DB

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,7 +30,7 @@ deleteButton.forEach(button => {
     const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
 
     modalTitle.innerHTML = `${text} esta pergunta`
